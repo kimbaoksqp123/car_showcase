b44c59b2bcf04123f7068e3f564530ea0790fc91
ds-bao-nguyen <bao-nguyen@dimage.co.jp>
2025-03-21 10:53:03 +0900

    search bar

--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
-import { SearchManufacturer } from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
 
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer };
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, CarCard, ShowMore };
